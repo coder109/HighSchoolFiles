b4096b23fe48ac3b249f8ab9b5268e8f9bcd02d6
Coder109 <3211697682@qq.com>
2020-07-31 13:53:55 +0800

    Update

--- python/杂项们/choujiang.py
+++ python/杂项们/choujiang.py
@@ -4,10 +4,16 @@
 import random as rd
 
 #MainProcedure
-rnum=rd.randint(0,10)
 
 #Loop
-if rnum==10:
-    print("SSR!")
-elif rnum<10:
-    print("LOSER!")
+i,n,r=0,0,1
+while r<=10000:
+    rnum=rd.randint(0,10000)
+    if rnum==10000:
+        #print("SSR!")
+        i=i+1
+    elif rnum<10000:
+        #print("LOSER!")
+        n=n+1
+    r=r+1
+print(str(i)+"   "+str(n))
