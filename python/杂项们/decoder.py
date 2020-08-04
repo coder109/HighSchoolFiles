115f9153ead2d5f540167f97a06100f61e764ff3
Coder109 <3211697682@qq.com>
2020-08-04 13:14:34 +0800

    Update

--- python/杂项们/decoder.py
+++ python/杂项们/decoder.py
@@ -1,22 +1,29 @@
 #-*-coding:UTF-8-*-
+#Modules
+import sys
+
 #Main
 #Input
 keyword=input("输入一级秘钥：")
 deblk=input("输入二级秘钥：")
 wrd=input("输入要解密的语句:")
 
-#Input2List
-keyword=list(keyword)
-deblk=deblk.split(" ")
-wrd=wrd.split(" ")
+#Judgement
+if len(keyword)==0 or len(deblk)==0 or len(wrd)==0:
+    sys.exit(1)
+else:
+    #Input2List
+    keyword=list(keyword)
+    deblk=deblk.split(" ")
+    wrd=wrd.split(" ")
 
-#Decode
-result=[]
-i=0
-while i<len(wrd):
-    res=int(wrd[i])/ord(keyword[int(deblk[i])])
-    result.append(chr(int(res)))
-    i=i+1
+    #Decode
+    result=[]
+    i=0
+    while i<len(wrd):
+        res=int(wrd[i])/ord(keyword[int(deblk[i])])
+        result.append(chr(int(res)))
+        i=i+1
 
-#Output
-print("解密结果："+"".join(result))
+    #Output
+    print("解密结果："+"".join(result))
