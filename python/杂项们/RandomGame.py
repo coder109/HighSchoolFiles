30ba85697e69e4cb279c7fab48420350409893df
Coder109 <3211697682@qq.com>
2020-10-03 12:12:27 +0800

    UPDATE

--- python/杂项们/RandomGame.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:UTF-8-*-
-import numpy as np
-s=0
-i=1
-while i<=2020:
-    temp=2*np.sin(i*np.pi/2/180+np.pi/3/180)
-    s=s+temp
-    i=i+1
-print(s)
--- python/杂项们/dialer.py
+++
@@ -1,6 +0,0 @@
-#-*-coding:UTF-8-*-
-#Modules
-import winsound
-
-#Main
-winsound.Beep(1000,2000)
