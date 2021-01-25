6f3c9051402a32407eb5618432a3b7c1c8b2e0f6
Coder109 <3211697682@qq.com>
2021-01-25 08:31:38 +0800

    update

--- python/pictures/pic.py
+++ python/pictures/pic.py
@@ -2,8 +2,8 @@
 from numpy import *
 import matplotlib.pyplot as plt
  
-x=linspace(0,10,5000)
-y=sin(x)**2*sin(2*x)
+x=linspace(0,10,50000)
+y=x/2-cos(x)-pi/4
 plt.figure()
 plt.plot(x,y)
 plt.savefig("easyplot.png")#导出图像入图片
