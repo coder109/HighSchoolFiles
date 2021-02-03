4a73bfcf1ebd6f74787b2c9e34adbda423664b53
Coder109 <3211697682@qq.com>
2021-02-03 09:50:53 +0800

    UPDATE

---
+++ python/杂项们/randomer.py
@@ -0,0 +1,10 @@
+# -*-coding:UTF-8-*-
+
+import random
+
+a=["Chinese","Math","English","Physics","Chemistry","Biology"]#Subject
+b=["Papers","Books"]
+
+i=random.randint(0,5)
+j=random.randint(0,1)
+print(a[i]+" "+b[j])
